7a5dba979ae205bffe9495bfaa96cc1210e03970
Varun Kumar <varunhisaria@gmail.com>
2015-11-01 00:57:00 +0530

    debugging sms

--- missed_call.php
+++ missed_call.php
@@ -1,8 +1,6 @@
 <?php
 	$caller=$_GET['From'];
 	header("Content-type:text/plain");
-	header("HTTP/1.1 200 OK");
-	header("Content-Length: 130");
-	$response =  "Thank you".$caller;
+	$response =  "Thank you";
 	echo $response;
 ?>
