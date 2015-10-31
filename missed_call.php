c81166d0929b86472d82ea6ab2a7ad9080ea166e
Varun Kumar <varunhisaria@gmail.com>
2015-11-01 00:43:24 +0530

    send sms

--- missed_call.php
+++ missed_call.php
@@ -1,3 +1,8 @@
 <?php
-	
+	$caller=$_GET['From'];
+	header("Content-type:text/plain");
+	header("HTTP/1.1 200 OK");
+	header("Content-Length: 130");
+	$response =  "Thank you".$caller;
+	echo $response;
 ?>
