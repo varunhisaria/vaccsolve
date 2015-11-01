0dd9d885f2212a42d9db2cd88e6d3ee7ce4d1ae5
Varun Kumar <varunhisaria@gmail.com>
2015-11-01 13:35:38 +0530

    phpmyadmin 4.0.10

--- phpMyAdmin/version_check.php
+++ phpMyAdmin/version_check.php
@@ -10,25 +10,48 @@
 define('PMA_MINIMUM_COMMON', true);
 require_once 'libraries/common.inc.php';
 require_once 'libraries/Util.class.php';
-require_once 'libraries/VersionInformation.php';
+
+// Get response text from phpmyadmin.net or from the session
+// Update cache every 6 hours
+if (isset($_SESSION['cache']['version_check'])
+    && time() < $_SESSION['cache']['version_check']['timestamp'] + 3600 * 6
+) {
+    $save = false;
+    $response = $_SESSION['cache']['version_check']['response'];
+} else {
+    $save = true;
+    $file = 'http://www.phpmyadmin.net/home_page/version.json';
+    if (ini_get('allow_url_fopen')) {
+        $response = file_get_contents($file);
+    } else if (function_exists('curl_init')) {
+        $curl_handle = curl_init($file);
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($curl_handle);
+    }
+}
 
 // Always send the correct headers
 header('Content-type: application/json; charset=UTF-8');
 
-$versionInformation = new VersionInformation();
-$versionDetails = $versionInformation->getLatestVersion();
-
-if (empty($versionDetails)) {
-    echo json_encode(array());
-} else {
-    $latestCompatible = $versionInformation->getLatestCompatibleVersion(
-        $versionDetails->releases
+// Save and forward the response only if in valid format
+$data = json_decode($response);
+if (is_object($data)) {
+    $latestCompatible = PMA_Util::getLatestCompatibleVersion(
+        $data->releases
     );
+
     $version = '';
     $date = '';
     if ($latestCompatible != null) {
         $version = $latestCompatible['version'];
         $date = $latestCompatible['date'];
+    }
+
+    if ($save) {
+        $_SESSION['cache']['version_check'] = array(
+            'response' => $response,
+            'timestamp' => time()
+        );
     }
     echo json_encode(
         array(
@@ -37,3 +60,5 @@ if (empty($versionDetails)) {
         )
     );
 }
+
+?>
